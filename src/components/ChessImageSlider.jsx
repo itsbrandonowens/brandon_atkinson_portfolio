1beb08a5f2a4b915c59e5cf9959d3e4cf3643323
itsbrandonowens <brandondavidowens@gmail.com>
2024-10-15 09:27:24 +0100

    mobile fix

--- src/components/ChessImageSlider.jsx
+++ src/components/ChessImageSlider.jsx
@@ -21,7 +21,8 @@ class ChessImageSlider extends React.Component {
         ],
         currentIndex: 0
     }; // The images to scroll through
-    
+
+    count = 1;
     componentDidMount() {
         this.startSlider();
         window.scrollTo({ top: 0, behavior: 'smooth' }) //starts the slider and scrolls the page to the top 
@@ -37,10 +38,12 @@ class ChessImageSlider extends React.Component {
             const nextIndex = (currentIndex + 1) % images.length;
             this.setState({ currentIndex: nextIndex });
         }, 3000);
+       
     }; // Change the duration (in milliseconds) between slides as needed
 
     stopSlider = () => {
         clearInterval(this.sliderInterval);
+        
     };
 
     footerHandler = () => {
@@ -49,22 +52,46 @@ class ChessImageSlider extends React.Component {
 
     prevHandler = () => {
         const { images, currentIndex } = this.state;
-        const prevIndex = (currentIndex -1);
+        const prevIndex = (currentIndex - 1);
         if (currentIndex > 0) {
 
-           this.setState({currentIndex:prevIndex})
+            this.setState({ currentIndex: prevIndex })
         }
         console.log("Prev");
     };
 
     nextHandler = () => {
         const { images, currentIndex } = this.state;
-        const nextIndex = (currentIndex +1);
-        if (currentIndex < (images.length-1)) {
+        const nextIndex = (currentIndex + 1);
+        if (currentIndex < (images.length - 1)) {
 
-           this.setState({currentIndex:nextIndex})
+            this.setState({ currentIndex: nextIndex })
         }
         console.log("Next");
+    };
+
+    pauseHandler = () => {
+        
+        //checking if the number is even or odd by checking remainder
+        let count = this.count;
+        const isOdd = count => this.count % 2 === 1;
+        const isEven = count => this.count % 2 === 0;
+        
+        if (isOdd(this.count)) {
+            this.stopSlider();
+            count++
+            if (isEven(this.count)) {
+                this.startSlider();
+                count++
+            }
+            //#####################
+            //FIX THIS
+            //########################
+            
+          
+        }   
+
+
     };
 
 
@@ -73,9 +100,9 @@ class ChessImageSlider extends React.Component {
     render() {
         const { images, currentIndex } = this.state;
         const currentImage = images[currentIndex];
-        
+        let count = this.count;
 
-        
+
         return (
             <motion.div
                 initial={{ opacity: 0 }}
@@ -88,15 +115,15 @@ class ChessImageSlider extends React.Component {
                     <a href="https://github.com/itsbrandonowens/brandons_simple_chess" target="_blank"><h1 className="p_detail_subtitle"> Java Project Code</h1></a>
 
                     <img className="p_image" src={currentImage} alt={`Image ${currentIndex}`} />
-                    <button className ="prev_button" onClick={this.prevHandler}> Prev </button>
-                    <button className ="next_button" onClick={this.nextHandler}> Next </button>
+                    <button className="prev_button" onClick={this.prevHandler}> Prev </button>
+                    <button className="next_button" onClick={this.nextHandler}> Next </button>
                     <div className="p_details_container">
-                    
+
                         <p className="p_details_text">
-                        I developed a fully functional chess game using Java, focusing on object-oriented programming principles. The game features all standard chess rules, 
-                        including check, checkmate, and stalemate. The user interface allows players to make moves via a grid-based board, highlighting valid moves for selected pieces.
-                         The backend handles complex game logic such as move validation, special rules (e.g., castling, en passant), and piece promotion. The project demonstrates my proficiency in Java and showcases 
-                         skills like problem-solving, algorithm design, and clean code practices.
+                            I developed a fully functional chess game using Java, focusing on object-oriented programming principles. The game features all standard chess rules,
+                            including check, checkmate, and stalemate. The user interface allows players to make moves via a grid-based board, highlighting valid moves for selected pieces.
+                            The backend handles complex game logic such as move validation, special rules (e.g., castling, en passant), and piece promotion. The project demonstrates my proficiency in Java and showcases
+                            skills like problem-solving, algorithm design, and clean code practices.
                         </p>
                         <p className="p_details_text">
                             The link to the code on GitHub can be found <a href="https://github.com/itsbrandonowens/brandons_simple_chess" target="_blank"> here</a> or above.
