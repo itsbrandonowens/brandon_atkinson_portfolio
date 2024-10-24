6209e938589dd9078fa5cc1fa75fa9b37a4d59c0
itsbrandonowens <brandondavidowens@gmail.com>
2024-10-24 20:27:49 +0100

    anchor tag fix again lol

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,11 +22,11 @@ const Navbar = () => {
             <p className="navbar_title_text">Software Developer</p>
             <h2 className="navbar_subtitles">Navigation</h2>
             <li className="navbar_list">
-                <a href="#top" className="no_list_style"> <ul className="navbar_item"> <img src={home_icon} className="navbar_icon" alt="home_icon"></img> <p className="navbar_text"> Explore </p></ul></a>
-                <a href="#about" className="no_list_style"> <ul className="navbar_item"> <img src={about_icon} className="navbar_icon" alt="about_icon"></img><p className="navbar_text"> About </p></ul></a>
-                <a href="#service" className="no_list_style"><ul className="navbar_item"> <img src={service_icon} className="navbar_icon" alt="service_icon"></img><p className="navbar_text"> Service </p></ul></a>
-                <a href="#portfolio" className="no_list_style"><ul className="navbar_item"> <img src={portfolio_icon} className="navbar_icon" alt="portfolio_icon"></img><p className="navbar_text"> Portfolio </p></ul></a>
-                <a href="#contact" className="no_list_style"><ul className="navbar_item"> <img src={contact_icon} className="navbar_icon" alt="contact_icon"></img><p className="navbar_text"> Contact </p></ul></a>
+                <a href="/brandon_atkinson_portfolio/#top" className="no_list_style"> <ul className="navbar_item"> <img src={home_icon} className="navbar_icon" alt="home_icon"></img> <p className="navbar_text"> Explore </p></ul></a>
+                <a href="/brandon_atkinson_portfolio/#about" className="no_list_style"> <ul className="navbar_item"> <img src={about_icon} className="navbar_icon" alt="about_icon"></img><p className="navbar_text"> About </p></ul></a>
+                <a href="/brandon_atkinson_portfolio/#service" className="no_list_style"><ul className="navbar_item"> <img src={service_icon} className="navbar_icon" alt="service_icon"></img><p className="navbar_text"> Service </p></ul></a>
+                <a href="/brandon_atkinson_portfolio/#portfolio" className="no_list_style"><ul className="navbar_item"> <img src={portfolio_icon} className="navbar_icon" alt="portfolio_icon"></img><p className="navbar_text"> Portfolio </p></ul></a>
+                <a href="/brandon_atkinson_portfolio/#contact" className="no_list_style"><ul className="navbar_item"> <img src={contact_icon} className="navbar_icon" alt="contact_icon"></img><p className="navbar_text"> Contact </p></ul></a>
             </li>
 
             <h2 className="navbar_subtitles">Socials</h2>
