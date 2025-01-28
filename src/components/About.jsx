57245b3916c512cb2a4bf52970ff30909c0a6262
itsbrandonowens <brandondavidowens@gmail.com>
2025-01-28 00:10:09 +0000

    about section update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
 import '../styles/About.css'
+import AboutPicture from './images/about_picture.jpg'
 
 const About = () => {
-/* Go In Photoshop and Create a dummy page  */
+
     return (
         <div className="about_container">
             <h1 className="about_title" id="about">ABOUT</h1>
+            <div className='about_grid_container'>
+                <img src={AboutPicture} className="about_image" alt="Brandon Atkinson Image"></img>
+                <p className="about_text">
+                    <span className='hello_text'> Hello! I'm Brandon, a Full-Stack Developer based in Newcastle Upon Tyne!</span>
+                    <br></br>
+                    <br></br>
+                    With experience in object-oriented programming and a commitment to continuous learning, I have developed and implemented a range of projects, including my online game Last Orders and several Spring Boot applications.
+                    <br></br>
+                    <br></br>
+                    I am a determined, passionate developer, driven by curiosity and creativity, constantly striving to learn, innovate, and deliver impactful solutions through my dedication to coding and problem-solving.
+                    <br></br>
+                    <br></br>
+                    I’m a big Newcastle United fan and passionate PC gamer who loves upgrading his hardware and staying updated on tech trends. When I’m not gaming or coding, I enjoy hitting the gym and staying active.
 
-            <p className="about_text"> Hello! I'm Brandon, a Full-Stack Software Developer based in Newcastle Upon Tyne. I’m a passionate software developer with a strong foundation in Java and a growing interest in full-stack development. With experience in object-oriented
-                programming and a commitment to continuous learning, I have developed and implemented a range of projects with a range of languages, including a fully functional chess game in Java and an
-                online game with ReactJS. I enjoy taking on new challenges and translating ideas into functional, user-friendly applications.
-            </p>
-            
-            <div className="about_grid">
-                <div className="about_grid_item">
-                    <h2 className="about_subtitle"> Tech Stack </h2>
-                    <p className="about_grid_text">
-                        <p className="navbar_text"> Java w/ Spring Boot, ReactJS, HTML/CSS, SQL </p>
-                    </p>
-                </div>
-                <div className="about_grid_item">
-                    <h2 className="about_subtitle"> Hobbies and Interests </h2>
-                    <p className="about_grid_text">
-                        I am a big Newcastle United fan, and football in general. I have an avid interest in PC Gaming, and upgrading my PC at home whenever I can.
-                        I love to go to the gym and play 5-aside with my friends whenever I get the chance.
-                    </p>
-                </div>
+                </p>
             </div>
-
         </div>
     )
 }
